c4fef31bb9f5100abefa41e70a13e58395486de8
boularbahsmail <qiranorane55@gmail.com>
2022-08-03 12:24:55 +0100

    Fix Subscription Form Margin > Media Queries

--- components/Subscription/index.jsx
+++ components/Subscription/index.jsx
@@ -16,7 +16,7 @@ const Subscription = () => {
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                     This last been the industrys standard dummy text ever since the 1500s,
                 </p>
-                <div className="flex flex-col justify-start items-center gap-2 max-w-xl md:flex-row">
+                <div className="flex flex-col justify-start items-center gap-2 max-w-xl w-100 m-auto lg:m-0 md:flex-row">
                     <input type="email" placeholder="Your_email@example.com" className="border border-gray-400 text-md font-semibold mb-1 max-w-full w-full outline-none rounded-md m-0 py-2 px-4 md:py-3 md:px-4 md:mb-0 focus:border-red-500" />
                     <Link href="">
                         <a className="w-full flex justify-center items-center gap-2 w-max m-auto py-2 px-4 bg-red-500 text-white font-bold border border-red-500 rounded-md ease-in-out duration-150 s00hadow-slate-6 hover:bg-white hover:text-red-500 lg:m-0 md:py-3 md:px-6 md:w-20" title="Subscribe">
