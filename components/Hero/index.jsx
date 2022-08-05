12e6335f85b75f8376d68fb1ce0fad04848d3f6a
boularbahsmail <qiranorane55@gmail.com>
2022-08-05 14:07:58 +0100

    Add /menu route to Order Now Hero Button (Link)

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 This last been the industrys standard dummy text ever since the 1500s,
             </p>
-            <Link href="">
+            <Link href="/menu">
                 <a className="flex justify-center items-center gap-2 w-max m-auto py-2 px-4 bg-red-500 text-white font-bold border border-red-500 rounded-md ease-in-out duration-150 shadow-slate-600 hover:bg-white hover:text-red-500 lg:m-0 md:py-3 md:px-6" title="Order Now">
                     <span>Order Now</span>
                     <span><GiHamburger /></span>
